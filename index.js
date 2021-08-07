793d463c836eab8e3d10a6d00f3172ef17b15d27
ZhukovMikhail <79285451+ZhukovMikhail@users.noreply.github.com>
2021-08-07 03:20:49 +0300

    timer done
    bomb is applied

--- index.js
+++ index.js
@@ -1,44 +1,72 @@
 //=========================== пишем таймер обратного отсчета =================
-const refs = {
-  Days: document.querySelector('[data-value="days"]'),
-  Hours: document.querySelector('[data-value="hours"]'),
-  Minutes: document.querySelector('[data-value="mins"]'),
-  Seconds: document.querySelector('[data-value="secs"]'),
-};
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.refs = {
+      Days: document.querySelector('[data-value="days"]'),
+      Hours: document.querySelector('[data-value="hours"]'),
+      Minutes: document.querySelector('[data-value="mins"]'),
+      Seconds: document.querySelector('[data-value="secs"]'),
+      timerId: document.querySelector('#timer-1'),
+      body: document.querySelector('body'),
+    };
+    this.intervalId = null;
+    this.days = null;
+    this.hours = null;
+    this.mins = null;
+    this.secs = null;
 
-let intervalId = null;
-let days = null;
-let hours = null;
-let mins = null;
-let secs = null;
-
-const targetDate = new Date(2021, 7, 7, 0, 57, 25, 0);
-const time = function () {
-  return targetDate.getTime() - Date.now();
-};
-
-startTimer(time);
-
-function startTimer() {
-  if (time() <= 0) {
-    refs.Days.textContent = '00';
-    refs.Hours.textContent = '00';
-    refs.Minutes.textContent = '00';
-    refs.Seconds.textContent = '00';
-    clearInterval(intervalId);
-    return;
+    this.time = function () {
+      return this.targetDate.getTime() - Date.now();
+    };
   }
-  intervalId = setInterval(() => {
-    days = Math.floor(time() / (1000 * 60 * 60 * 24));
-    hours = Math.floor((time() % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    mins = Math.floor((time() % (1000 * 60 * 60)) / (1000 * 60));
-    secs = Math.floor((time() % (1000 * 60)) / 1000);
 
-    refs.Days.textContent = `${days}`;
-    refs.Hours.textContent = `${hours}`;
-    refs.Minutes.textContent = `${mins}`;
-    refs.Seconds.textContent = `${secs}`;
+  startTimer() {
+    this.intervalId = setInterval(() => {
+      if (this.time() <= 0) {
+        this.refs.Days.textContent = '00';
+        this.refs.Hours.textContent = '00';
+        this.refs.Minutes.textContent = '00';
+        this.refs.Seconds.textContent = '00';
+        clearInterval(this.intervalId);
+        this.refs.timerId.insertAdjacentHTML(
+          'beforebegin',
+          '<h1 class="text">Time is up, you didn`t make it!!!</h1><h2>!!--=BOOM=--!!</h2>',
+        );
+        this.refs.body.classList.add('bg-body');
+        return;
+      }
+      this.refs.body.classList.remove('bg-body');
+      this.days = Math.floor(this.time() / (1000 * 60 * 60 * 24));
+      this.hours = Math.floor(
+        (this.time() % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+      );
+      this.mins = Math.floor((this.time() % (1000 * 60 * 60)) / (1000 * 60));
+      this.secs = Math.floor((this.time() % (1000 * 60)) / 1000);
 
-    console.log(`дней ${days}, часов ${hours}, минут ${mins}, секунд ${secs}`);
-  }, 1000);
+      this.refs.Days.textContent =
+        this.days < 10 ? `0 ${this.days}` : `${this.days}`;
+      this.refs.Hours.textContent =
+        this.hours < 10 ? `0 ${this.hours}` : `${this.hours}`;
+      this.refs.Minutes.textContent =
+        this.mins < 10 ? `0 ${this.mins}` : `${this.mins}`;
+      this.refs.Seconds.textContent =
+        this.secs < 10 ? `0 ${this.secs}` : `${this.secs}`;
+
+      console.log(
+        `дней ${this.days}, часов ${this.hours}, минут ${this.mins}, секунд ${this.secs}`,
+      );
+    }, 1000);
+  }
 }
+
+//================= new timer ================= //
+const test = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Aug 17, 2021'),
+  targetDate: new Date(2021, 7, 7, 3, 17, 45, 0),
+});
+
+test.startTimer();
+// console.log(test.time());
